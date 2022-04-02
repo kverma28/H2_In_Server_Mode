34ea6065a9617c19ab90031ed8cb6ab90a65854c
kverma28 <kverma28@gmail.com>
2022-04-02 13:39:30 +0530

    code commit

--- src/main/java/com/examples/App.java
+++ src/main/java/com/examples/App.java
@@ -1,9 +1,8 @@
 package com.examples;
 
 import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
 import org.h2.tools.Server;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -14,24 +13,24 @@ import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
 
+@Slf4j
 @SpringBootApplication
 public class App {
     public static void main(String[] args) throws SQLException {
-        Logger logger = LoggerFactory.getLogger(App.class);
         ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
 
         DataSource hikariDS = context.getBean(HikariDataSource.class);
         Connection connection = hikariDS.getConnection();
-        logger.info("Database Schema : {}", connection.getSchema());
+        log.info("Database Schema : {}", connection.getSchema());
 
         DatabaseMetaData metaData = connection.getMetaData();
 
-        logger.info("Database Driver Name : {}", metaData.getDriverName());
-        logger.info("Database User Name : {}", metaData.getUserName());
-        logger.info("Database product name : {}", metaData.getDatabaseProductName());
-        logger.info("Database Connection URL for embedded mode : {}", metaData.getURL());
-        logger.info("Database Connection URL for server mode : jdbc:h2:tcp://localhost:{}/mem:testdb", context.getEnvironment().getProperty("h2.port"));
-        logger.info("H2 console can be accessed at : http://localhost:{}/h2-console", context.getEnvironment().getProperty("server.port"));
+        log.info("Database Driver Name : {}", metaData.getDriverName());
+        log.info("Database User Name : {}", metaData.getUserName());
+        log.info("Database product name : {}", metaData.getDatabaseProductName());
+        log.info("Database Connection URL for embedded mode : {}", metaData.getURL());
+        log.info("Database Connection URL for server mode : jdbc:h2:tcp://localhost:{}/mem:testdb", context.getEnvironment().getProperty("h2.port"));
+        log.info("H2 console can be accessed at : http://localhost:{}/h2-console", context.getEnvironment().getProperty("server.port"));
     }
 
     @Bean(initMethod = "start", destroyMethod = "stop")
